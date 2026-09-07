7f7266499e0c68879691d118a48f478eaf873cb6
Luis-Pascal Mack <luis@lmck.eu>
2026-09-08 01:35:40 +0200

    content update WiSe26

--- pictures/update-erstiheft.sh
+++ pictures/update-erstiheft.sh
@@ -3,5 +3,9 @@
 # ⚠ This script requires qpdf ⚠
 
 wget https://teri.fsi.uni-tuebingen.de/anfiheft/anfiheft-info.pdf
-qpdf anfiheft-info.pdf --pages . 1-1 -- erstiheft.pdf
+qpdf anfiheft-info.pdf --pages . 1-1 -- erstiheft-info.pdf
 rm anfiheft-info.pdf anfiheft-info.pdf.1
+
+wget https://teri.fsi.uni-tuebingen.de/anfiheft/anfiheft-kogni.pdf
+qpdf anfiheft-kogni.pdf --pages . 1-1 -- erstiheft-kogni.pdf
+rm anfiheft-kogni.pdf anfiheft-kogni.pdf.1
